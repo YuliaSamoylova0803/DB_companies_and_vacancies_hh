622042d6be1edc9891954108f082b071d9624e66
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-03-01 23:27:47 +0500

    update module employerparser and add tests

--- src/employerparser.py
+++ src/employerparser.py
@@ -1,15 +1,28 @@
 from abc import ABC, abstractmethod
+from pathlib import Path
 from typing import Any
 
 import requests
 
+from setting import BASE_DIR
+
+json_filename = Path(BASE_DIR, "data", "employers_data.json").parent
+database_path = Path(BASE_DIR, "src", "database.ini").parent
+
+
 class Parser(ABC):
     """Абстрактный класс для работы с API сервиса с работодателями"""
+
     pass
 
     @abstractmethod
     def load_employers(self):
         """Метод отправки get-запроса на сайт Head Hunter"""
+        pass
+
+    @abstractmethod
+    def filtered_by_the_number_of_vacancies(self):
+        """Метод получения компаний только с открытыми вакансиями"""
         pass
 
 
@@ -26,22 +39,29 @@ class EmployerParser(Parser):
     def load_employers(self, keyword) -> Any:
         """Метод отправки get-запроса на сайт Head Hunter"""
         self.params["text"] = keyword
-        while self.params.get("page") != 30:
+        while self.params.get("page") != 50:
             response = requests.get(self._url, headers=self._headers, params=self.params)
-            vacancies = response.json()["items"]
-            self.employers.extend(vacancies)
+            employers = response.json()["items"]
+            self.employers.extend(employers)
             self.params["page"] += 1
         return self.employers
 
+    def filtered_by_the_number_of_vacancies(self, employers: list[dict[str, Any]]) -> list[dict[str, Any]]:
+        """Метод получения компаний только с открытыми вакансиями"""
+        employers_list = []
+        for employer in self.employers:
+            if employer.get("open_vacancies", 0) > 0:
+                employers_list.append(employer)
+        return employers_list
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     hh_api = EmployerParser()
     print(hh_api)
 
     # Получение работодателей с hh.ru в формате JSON
-    hh_employers = hh_api.load_employers("СБЕР")
-    print(hh_employers)
-    print(len(hh_employers))
-
-
-
+    employers = hh_api.load_employers("")
+    print(employers)
+    print(len(employers))
+    search_employers = employers.filtered_by_the_number_of_vacancies(employers)
+    print(search_employers)
